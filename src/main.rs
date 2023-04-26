53c95dc8ca86a1a558456c38bc54a3c0b2519f1b
grayjiang <Gray_Jy@yeah.net>
2023-04-26 17:52:50 +0800

    feat: 数值类型

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,36 @@
 fn main() {
-    about_unused_variable();
-    about_bind_variability_shadowing_scope();
-    about_deconstruction();
+    about_number()
 }
 
 /**
- * 未使用变量 & 格式化占位符
- * 注意函数名命名格式 a_b_c
- * 未使用变量通过_variable声明，避免warning
- * 关于 {} (显示格式化占位符) 和 {:?} (调试打印格式化占位符) 的区别：
- * {} 输出方式取决于类型实现的 std::fmt::Display trait
- * {:?}基于 Debug trait 实现的，用于将一个值格式化为可供调试输出的形式。对于 Rust 内建类型，一般会以比较友好的方式输出；
- * 可以通过 #[derive(Debug)] 来手动实现Debug trait，继而可以使用{:?}
- */
-fn about_unused_variable() {
-    let x: i32 = 2;
-    let _y: i32 = 4;
-    println!("x is {}", x);
-    println!("x is {:?}", x);
-    let s = String::from("hello");
-    println!("Value of s is {}", s); // hello 可以看出，使用 {} 占位符输出时，字符串 s 的引号被去掉了。它以更加自然的形式进行输出
-    println!("Value of s is {:?}", s); // "hello"
-    let v = vec![1, 2, 3];
-    println!("Value of v is {:?}", v);
-    // println!("Value of v is {}", v); // error:因为Vec类型没有实现`std::fmt::Display`
+* 分为整数类型和浮点数类型(f32 f64) 默认浮点类型是 f64，在现代的 CPU 中它的速度与 f32 几乎相同，但精度更高。
+* 其中整数类型分为有符号整数(i)，无符号整数(u)
+* 每个有符号类型规定的数字范围是 -(2n - 1) ~ 2n - 1 - 1
+* 无符号类型可以存储的数字范围是 0 ~ 2n - 1
+* i8 --> -128 至 127  u8 --> 0 至 255
+* i16 --> -32,768 至 32,767 u16 --> 0 至 65535
+* i32 --> -2,147,483,648 至 2,147,483,647 u32 -->  0 至 4,294,967,295 Rust默认数值类型为i32
+* i64 -->  -9,223,372,036,854,775,808 至 9,223,372,036,854,775,807 u64 --> 0 至 18,446,744,073,709,551,615
+* isize / usize --> 视架构而定：取决于程序运行的计算机 CPU 类型： 若 CPU 是 32 位的，则这两个类型是 32 位的，同理，若 CPU 是 64 位，那么它们则是 64 位。isize 和 usize 的主要应用场景是用作集合的索引。
+* 关于整形溢出问题：
+假设有一个 u8 ，它可以存放从 0 到 255 的值。那么当你将其修改为范围之外的值，比如 256，则会发生整型溢出。关于这一行为 Rust 有一些有趣的规则：当在 debug 模式编译时，Rust 会检查整型溢出，若存在这些问题，则使程序在编译时 panic(崩溃,Rust 使用这个术语来表明程序因错误而退出)。
 
-    #[derive(Debug)] // 手动实现Debug trait
+在当使用 --release 参数进行 release 模式构建时，Rust 不检测溢出。相反，当检测到整型溢出时，Rust 会按照补码循环溢出（two’s complement wrapping）的规则处理。简而言之，大于该类型最大值的数值会被补码转换成该类型能够支持的对应数字的最小值。比如在 u8 的情况下，256 变成 0，257 变成 1，依此类推。程序不会 panic，但是该变量的值可能不是你期望的值。依赖这种默认行为的代码都应该被认为是错误的代码。
+要显式处理可能的溢出，可以使用标准库针对原始数字类型提供的这些方法：
 
-    struct Person {
-        name: String,
-        age: i32,
-    }
-    let nick = Person {
-        name: "nick".to_string(),
-        age: 12,
-    };
-    // println!("nick is {}", Nick); // error:因为Person没有实现`std::fmt::Display`
-    println!("nick is {:?}", nick); // 如果没有 #[derive(Debug)] 则 error:因为Person没有实现`Debug`
-                                    // 以下结构体.xx的输出直接可以使用,因为它们所代表的类型都默认实现了Display和Debug
-    println!("nick name is {}", nick.name);
-    println!("nick age is {}", nick.age);
-    println!("nick name is {:?}", nick.name);
-    println!("nick age is {:?}", nick.age);
-}
-
-/**
- * 变量绑定 & 变量可见性 & 变量遮蔽 & 变量作用域
- */
-fn about_bind_variability_shadowing_scope() {
-    let x: i32 = 1; // 将i32类型的1绑定到变量x上
-                    // x = 3; error:因为x不是可变的
-    let mut y: i32 = 1;
-    // y = "e"; // error:需要注意 mut的可变 意思是值的可变而不是类型的可变，一旦确定了类型，只能在这个类型范围内可变
-    y = 3;
-    y += 1;
-    println!("y is {}", y); // 4
-    let mut z: i32 = 1; // warning：这个变量z未被使用 因为被下面的同名变量z遮蔽了
-    let mut z = "xx";
-    // {} 为作用域 作用域内的变量只在当前作用域内生效，不会影响到作用域外
-    {
-        let mut z = 100;
-        println!("scope z is {}", z); // 100
-    }
-    println!("z is {}", z); // xx
-}
-
-/**
- * 变量解构 & 解构赋值
- */
-fn about_deconstruction() {
-    let (mut x, y) = (1, 2);
-    x = 4;
-    println!("x is {}", x); // 4
-    println!("y is {}", y); // 2
-    let (a, b): (i32, i32);
-    // (a, _) = (1, 2); // 也可以 但是2没有意义
-    // (a, _) = (1, _); // error:_只能用在左边
-    (a, _) = (1, ..);
-    println!("a is {}", a); // 1
-    [_, b] = [1, 3];
-    println!("b is {}", b); // 3
-    let (x, y, z): (i32, i32, i32);
-    (x, ..) = (1, ..);
-    println!("x is {}", x); // 1
+使用 wrapping_* 方法在所有模式下都按照补码循环溢出规则处理，例如 wrapping_add
+如果使用 checked_* 方法时发生溢出，则返回 None 值
+使用 overflowing_* 方法返回该值和一个指示是否存在溢出的布尔值
+使用 saturating_* 方法使值达到最小值或最大值
+*/
+fn about_number() {
+    let a: u8 = 255;
+    let b = a.wrapping_add(20);
+    println!("b {}", b); // 19
+    let c = a.checked_add(20);
+    println!("c {:?}", c); // None
+    let d = a.overflowing_add(20);
+    println!("d {:?}", d); // (19,true) 代表溢出了19
+    let e = a.saturating_add(20);
+    println!("e {}", e); // 达到最大值255
 }
